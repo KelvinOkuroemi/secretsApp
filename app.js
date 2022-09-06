0a0e7fa9527b0dce1f65656f758b372fef7fa97e
Kelvin Okuroemi <okuroemidouebikelvin@gmail.com>
2022-09-06 07:55:39 +0100

    push code to remote repo

--- app.js
+++ app.js
@@ -1,75 +1,178 @@
 //jshint esversion:6
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash")
+const _ = require("lodash");
 const app = express();
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const secret = process.env.SECRET;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate")
+// App setup
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Mongoose setup
-const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
-
-// App setup
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 // Database features
 const userSchema = new mongoose.Schema({
-    email : String,
-    password : String
-})
+  email: String,
+  password: String,
+  googleId: String,
+  secret : String
+});
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate)
+const User = mongoose.model("User", userSchema);
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields : ["password"]})
+passport.use(User.createStrategy());
 
-const User = mongoose.model("User",userSchema)
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.displayName });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret:process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+},
+(accessToken, refreshToken, profile, cb)=>{
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
 
 // App features
-app.get("/",(req,res)=>{
-    res.render("home")
-})
-
-app.get("/login",(req,res)=>{
-    res.render("login")
-})
-
-app.get("/register",(req,res)=>{
-    res.render("register")
-})
-
-app.post("/register",(req,res)=>{
-    const newUser = new User({
-        email : req.body.username,
-        password : req.body.password
-    })
-    newUser.save((err)=>{
-        if(err){
-            console.log(err)
-        }else{
-            res.render("secrets")
-        }
-    })
-})
-
-app.post("/login",(req,res)=>{
-    const username = req.body.username
-    const password = req.body.password
-
-    User.findOne({email : username},(err,foundUser)=>{
-        if(err){
-            console.log(err)
-        }else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets")
-                }
-            }
-        }
-    })
-})
-// App listen
-app.listen(3000, ()=> {
-   console.log("Server started on port 3000");
+app.get("/", (req, res) => {
+  res.render("home");
 });
+
+// Register with google
+app.get("/auth/google",passport.authenticate("google",{ scope: ["profile"] })
+)
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res)=> {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/secrets", (req, res) => {
+  User.find({"secret":{$ne : null}},(err,foundUsers)=>{
+    if(err){
+      console.log(err)
+    }else{
+      if(foundUsers){
+        res.render("secrets",{usersWithSecrets : foundUsers})
+      }
+    }
+  })
+});
+
+app.get("/submit", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+app.post("/submit",(req,res)=>{
+  const submittedSecret = req.body.secret
+  //Check if there is a user 
+  User.findById(req.user.id,(err,foundUser)=>{
+    if(err){
+      console.log()
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret
+        foundUser.save(()=>{
+          res.redirect("/secrets")
+        })
+      }
+    }
+  })
+})
+
+// Register with password and username
+app.post("/register", (req, res) => {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, newUser) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
+
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+// App listen
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
+// In this app, I learnt how to use hashing methods such as MD5, bcrypt and passport.js
+// The methods I learnt about was encryption, hashing and salting, and the use of environment variables
